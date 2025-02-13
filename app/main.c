e5294b3cd557897d96443dff5ed8541398e2c593
Gabriella Lord <gabriellalord@icloud.com>
2025-02-13 12:09:14 -0700

    Implemented modular heartbeat using TB0 overflow

--- app/main.c
+++ app/main.c
@@ -1,16 +1,27 @@
+#include "intrinsics.h"
+#include "msp430fr2355.h"
 #include <msp430.h>
 #include <stdbool.h>
+#include "heartbeat.h"
+
+void init()
+{
+    init_heartbeat();
+}
 
 int main(void)
 {
     // Stop watchdog timer
     WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
-    
-    // Disable the GPIO power-on default high-impedance mdoe to activate
-    // previously configure port settings
     PM5CTL0 &= ~LOCKLPM5;
+
+    init();
+    
 
     while(true)
     {
     }
+    return 0;
 }
+
+
